c42f890032358450e42fe1b5d19a2c8dddd744ce
Remedeae <sanna.m.blomdahl@gmail.com>
2024-10-23 22:39:49 +0200

    added exercise 3

--- index.js
+++ index.js
@@ -8,3 +8,17 @@ function generateRandomArray(length) {
     return randomArray;
 }
 console.log(generateRandomArray(30));
+
+//Exercise 3
+
+function sumEvenNumbers(array) {
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] % 2 === 0) {
+            sum += array[i];
+        }
+    }
+    return sum;
+}
+
+console.log(sumEvenNumbers(generateRandomArray(30)))
